4cb352574d242798a55c1cdb5f8727b2d17f6479
David Nguyen <david.nguyen@leadspicker.com>
2023-04-08 21:14:13 +0200

    initial commit

---
+++ server/urls.py
@@ -0,0 +1,36 @@
+"""server URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+import json
+
+from django.contrib import admin
+from django.urls import path
+
+from ninja_extra import NinjaExtraAPI
+from ninja_jwt.controller import NinjaJWTDefaultController
+from ninja_jwt.authentication import JWTAuth
+
+api = NinjaExtraAPI()
+api.register_controllers(NinjaJWTDefaultController)
+
+
+@api.get("/auth/me", auth=JWTAuth())
+def auth(request):
+    return {"is_authenticated": True}
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("api/", api.urls),
+]
